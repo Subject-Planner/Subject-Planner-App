9792495ce9b57513692d3c2cd707e0d83eecb2a5
mohammadmu1 <M_Mustafa9@outlook.com>
2023-12-14 13:43:15 +0300

    details-with-aws

--- app/src/main/java/com/demo/subjectplanner/activity/SubjectDetailsActivity.java
+++ app/src/main/java/com/demo/subjectplanner/activity/SubjectDetailsActivity.java
@@ -2,15 +2,38 @@ package com.demo.subjectplanner.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.demo.subjectplanner.R;
 
 public class SubjectDetailsActivity extends AppCompatActivity {
+    TextView subjectNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_details);
+
+        Intent callingIntent = getIntent();
+        String subjectTilteString = null;
+
+
+        if(callingIntent != null){
+            subjectTilteString = callingIntent.getStringExtra(MainActivity.SUBJECT_TITLE_TAG);
+
+
+             subjectNameTextView = (TextView) findViewById(R.id.subjectTitleText);
+
+
+            if (subjectTilteString != null){
+                subjectNameTextView.setText(subjectTilteString);
+            }else {
+                subjectNameTextView.setText("Subject Unknown");
+            }
+
+
+        }
     }
 }
--- app/src/main/java/com/demo/subjectplanner/activity/adapter/HomePageRecyclerViewAdapter.java
+++ app/src/main/java/com/demo/subjectplanner/activity/adapter/HomePageRecyclerViewAdapter.java
@@ -1,9 +1,11 @@
 package com.demo.subjectplanner.activity.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.amplifyframework.datastore.generated.model.Subject;
 import com.demo.subjectplanner.R;
+import com.demo.subjectplanner.activity.MainActivity;
+import com.demo.subjectplanner.activity.SubjectDetailsActivity;
 
 import java.util.List;
 
@@ -43,6 +47,25 @@ public class HomePageRecyclerViewAdapter extends RecyclerView.Adapter<HomePageRe
 
         // For example, if you have a description field in your Subject class:
        //  subjectDescTextView.setText(subject.getStartTime().toString());
+
+
+//        TextView taskFragmentTextView = (TextView) holder.itemView.findViewById(R.layout.cards_view);
+        String subjectTitle = subjects.get(position).getTitle();
+//        String taskBody = tasks.get(position).getDescription();
+//        String taskState = String.valueOf(tasks.get(position).getTaskStatusEnum());
+//        taskFragmentTextView.setText(position +" - "+ taskName);
+
+       ;
+        View productViewHolder = holder.itemView;
+        Button btn = (Button) holder.itemView.findViewById(R.id.cardViewButton);
+        btn.setOnClickListener(view -> {
+
+            Intent goToOrderFormIntent = new Intent(callingActivity, SubjectDetailsActivity.class);
+            goToOrderFormIntent.putExtra(MainActivity.SUBJECT_TITLE_TAG,  subjectTitle);
+//            goToOrderFormIntent.putExtra(MainActivity.TASK_BODY_TAG,  taskBody);
+//            goToOrderFormIntent.putExtra(MainActivity.TASK_STATE_TAG, taskState);
+            callingActivity.startActivity(goToOrderFormIntent);
+        });
 
     }
 
